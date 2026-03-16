9a392b95865b9e3d00d7459548f3ea0b171710d0
Jamie <jamierwaugh@gmail.com>
2026-03-16 13:21:28 +0000

    Improved casing and docstrings

--- messy.py
+++ messy.py
@@ -1,21 +1,34 @@
-x = 10
-y = 20
-z = x+y
+X = 10
+Y = 20
+Z = X+Y
 
+"""
+Adds numbers and returns them
+"""
 def add_numbers(a,b):
     return a+b
 
+"""
+Greets the user with a hello message
+"""
 def greet(name):
     print("Hello, " + name + "!")
 
-class myClass:
+"""
+Class used to store user name and age
+"""
+class MyClass:
     def __init__(self,name,age):
         self.name=name
         self.age=age
-    
-    def GetName(self):
+    def get_name(self):
         return self.name
+    def get_age(self):
+        return self.age
 
+"""
+Returns division of a and b, with a 0 check
+"""
 def divide(a, b):
     if b != 0:
         return "Error"
