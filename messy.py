05fa67f65b15b06ba3e4a055d7d0e7ebaad93c6f
Jamie <jamierwaugh@gmail.com>
2026-03-16 13:26:39 +0000

    Hopefully fixed all issues now

--- messy.py
+++ messy.py
@@ -1,39 +1,34 @@
+"""Messy python code to test pylint github action"""
 X = 10
 Y = 20
 Z = X+Y
 
 
 def add_numbers(a,b):
-    """
-    Adds numbers and returns them
-    """
+    """Adds numbers and returns them"""
     return a+b
 
 
 def greet(name):
-    """
-    Greets the user with a hello message
-    """
+    """Greets the user with a hello message"""
     print("Hello, " + name + "!")
 
 
 class MyClass:
-    """
-    Class used to store user name and age
-    """
+    """Class used to store user name and age"""
     def __init__(self,name,age):
         self.name=name
         self.age=age
     def get_name(self):
+        """Gets name of object"""
         return self.name
     def get_age(self):
+        """Gets age of object"""
         return self.age
 
 
 def divide(a, b):
-    """
-    Returns division of a and b, with a 0 check
-    """
+    """Returns division of a and b, with a 0 check"""
     if b != 0:
         return "Error"
     return a/b
